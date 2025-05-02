b5a00beb3510604e7b56beb1bf0043bd93366bb9
swathi-57 <110708836+swathi-57@users.noreply.github.com>
2025-05-02 09:38:02 +0530

    Add files via upload

---
+++ src/main/java/service/TaskService.java
@@ -0,0 +1,22 @@
+package taskmanagementsystem.service;
+
+import taskmanagementsystem.dto.ApiResponse;
+import taskmanagementsystem.model.Task;
+
+import java.util.List;
+
+public interface TaskService {
+    ApiResponse createTask(Task task, Long userId);
+
+    ApiResponse getTaskById(Integer taskId);
+
+    List<Task> getAllTasks(Long userId);
+
+    ApiResponse updateTask(Task task, Integer id);
+
+    public void deleteTask(Integer id);
+
+    ApiResponse doneTask(Integer id);
+
+    ApiResponse pendingTask(Integer id);
+}
